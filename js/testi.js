73bf30dcbb1066fc88a446c51f09d573159e88eb
ahmadputraridwan <a_putra.16411059@student.ubl.ac.id>
2023-11-15 23:54:57 +0700

    new-day7

--- js/testi.js
+++ js/testi.js
@@ -1,72 +1,72 @@
-const testi = [
-{
-  name : 'Ahmad',
-  quote : 'baik sekali',
-  ratting : 5,
-  image : '/images/wisuda.jpeg'
-},
-{
-  name : ' Putra',
-  quote : 'baik',
-  ratting : 4,
-  image : '/images/sepeda.jpeg'
-},
-{
-  name : 'Ridwan',
-  quote : 'cukup baik!',
-  ratting : 3,
-  image : '/images/fotosaya.jpeg'
-},
-{
-  name : 'Susilo',
-  quote : 'lumayan!',
-  ratting : 2,
-  image : '/images/rame.jpeg'
-},
-{
-  name : 'jek',
-  quote : 'buruk sekali!',
-  ratting : 1,
-  image : '/images/jek.jpeg'
-},
-]
-function rattingAll(){
-  let rattingAllHtml = '';
-
-  testi.forEach(item => {
-    rattingAllHtml += `
-    <div class="testimoni">
-              <img src=${item.image} alt="testi" class="profile-testi">
-              <p class="quote"> "${item.quote}" </p>
-              <p class="author"> ${item.name}</p>
-              <p class="ratting-5"><i class="fa-solid fa-star">${item.ratting} </i>
-               </p>
-            </div>`;
-  });
-  document.getElementById('testimonial').innerHTML = rattingAllHtml
-}
-rattingAll()
-
-
-// filtering function
-function selectRatting(ratting) {
-  let rattingAllHtml = ""
-
-  const dataFilter = testi.filter((item) => item.ratting === ratting)
-
-  if(dataFilter.length === 0) {
-      rattingAllHtml = `<h3>Data not found!</h3>`
-  } else {
-      dataFilter.forEach(item => {
-          rattingAllHtml += `
-          <div class="testimoni">
-              <img src=${item.image} alt="testi" class="profile-testi">
-              <p class="quote"> ${item.quote} </p>
-              <p class="author"> ${item.name}</p>
-              <p class="ratting-5"><i class="fa-solid fa-star">${item.ratting} </i>
-               </p>
-            </div>`;
-      })
+class cardTesti {
+  constructor(author, quote, image) {
+    this.author = author;
+    this.quote = quote;
+    this.image = image;
   }
-  document.getElementById('testimonial').innerHTML = rattingAllHtml
+
+  html() {
+    return `
+    <div class="testimoni">
+    <img src="${this.image}" alt="testi" class="profile-testi">
+    <p class="quote"> "${this.quote}" </p>
+    <p class="author"> '${this.author}'</p>
+    <p class="ratting-5"><i class="fa-solid fa-star">5 </i>
+     </p>
+  </div>
+        `;
+  }
 }
+
+const dataDummy = [
+  {
+    author : 'Ahmad',
+    quote : 'baik sekali',
+    image : '/images/wisuda.jpeg',
+  },
+  {
+  author : ' Putra',
+    quote : 'baik',
+    image : '/images/sepeda.jpeg'
+  },
+  {
+  author : 'Ridwan',
+    quote : 'cukup baik!',
+    image : '/images/fotosaya.jpeg',
+  },
+  {
+    author: 'Susilo',
+    quote : 'lumayan!',
+    image : '/images/rame.jpeg',
+  },
+];
+
+const card1 = new cardTesti(
+  dataDummy[0].author,
+  dataDummy[0].quote,
+  dataDummy[0].image
+);
+const card2 = new cardTesti(
+  dataDummy[1].author,
+  dataDummy[1].quote,
+  dataDummy[1].image
+);
+const card3 = new cardTesti(
+  dataDummy[2].author,
+  dataDummy[2].quote,
+  dataDummy[2].image
+);
+const card4 = new cardTesti(
+  dataDummy[3].author,
+  dataDummy[3].quote,
+  dataDummy[3].image
+);
+
+const testimonial = [card1, card2, card3, card4];
+
+let cardHtml = "";
+for (let x = 0; x < testimonial.length; x++) {
+  cardHtml += testimonial[x].html();
+}
+
+document.getElementById("testimonial").innerHTML = cardHtml;
