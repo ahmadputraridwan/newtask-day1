7b3a7558abb3bc930ce63f95fc19381139d9e5cb
ahmadputraridwan <a_putra.16411059@student.ubl.ac.id>
2023-11-29 16:09:39 +0700

    day12

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const path = require ('path')
 const app = express()
 const port = 4000
 const hbs = require('hbs')
-const { title } = require('process')
 
 // app.set = buat setting variabel global, configuration dll
 app.set("view engine", "hbs")
@@ -43,85 +42,114 @@ function contact(req,res){
 function blogDetail(req,res){
   const { id } = req.params // destructuring
 
-  const pName = ''
-  const description = ''
-  //  console.log('data id', id) //untuk menangkap id
+  const dataFilter = data[parseInt(id)]
+  dataFilter.id = parseInt(id)
 
-    const data = {
-        id,
-        pName,
-       description,
-    }
+    // const data =  {
+    //   id,
+    //   pName,
+    //   duration,
+    //   description,
+    //   js,
+    //   java,
+    //   phyton,
+    //   node
+    // }
+      
 
-  res.render('blog-detail', {data})
+  res.render('blog-detail', {data: dataFilter})
 }
 function addBlog(req,res){
   res.render('add-blog')
 }
 function addBlogViews(req, res){
-  const { pName, description, startDate, endDate, Technologies } = req.body;
+  const { pName, description, startDate, endDate, } = req.body;
 
-console.log('req body', req.body)
 
-let startDateValue = new Date(startDate);
-let endDateValue = new Date(endDate);
+  let startDateValue = new Date(startDate);
+  let endDateValue = new Date(endDate);
 
-let durasiWaktu = endDateValue.getTime() - startDateValue.getTime();
-let durasiHari = durasiWaktu / (1000 * 3600 * 24 );
-let durasiMinggu = Math.floor(durasiHari / 7);
-let sisaHari = durasiHari % 7 ;
+  let durasiWaktu = endDateValue.getTime() - startDateValue.getTime();
+  let durasiHari = durasiWaktu / (1000 * 3600 * 24);
+  let durasiMinggu = Math.floor(durasiHari / 7);
+  let sisaHari = durasiHari % 7;
 
-let durasiBulan = Math.floor (durasiHari  / 30);
-let sisaHariBulan = durasiHari % 30;
+  let durasiBulan = Math.floor(durasiHari / 30);
+  let sisaHariBulan = durasiHari % 30;
 
-let durasiTahun = Math.floor(durasiHari / 365 );
-let sisaHariTahun = durasiHari % 365;
+  let durasiTahun = Math.floor(durasiHari / 365);
+  let sisaHariTahun = durasiHari % 365;
 
-let duration = '';
+  let duration = "";
 
-if (durasiHari <= 6) {
-  duration = ` Durasi ${durasiHari + 1} Hari`;
-} else if (durasiTahun > 0) {
-  if (sisaHariTahun > 0) {
-    duration = `Durasi ${durasiTahun} Tahun ${Math.floor(
-      sisaHariTahun / 30
-    )} Bulan ${Math.floor(sisaHariTahun % 30)} Hari`;
+  if (durasiHari <= 6) {
+    duration = ` Durasi ${durasiHari + 1} Hari`;
+  } else if (durasiTahun > 0) {
+    if (sisaHariTahun > 0) {
+      duration = `Durasi ${durasiTahun} Tahun ${Math.floor(
+        sisaHariTahun / 30
+      )} Bulan ${Math.floor(sisaHariTahun % 30)} Hari`;
+    } else {
+      duration = `Durasi ${durasiTahun} Tahun`;
+    }
+  } else if (durasiBulan > 0) {
+    if (sisaHariBulan > 0) {
+      duration = `Durasi ${durasiBulan} Bulan ${sisaHari - 1} Hari`;
+    } else {
+      duration = `Durasi ${durasiBulan} Bulan`;
+    }
+  } else if (sisaHari > 0) {
+    duration = `Durasi ${durasiMinggu} Minggu ${sisaHari} Hari`;
   } else {
-    duration = `Durasi ${durasiTahun} Tahun`;
+    duration = `Durasi ${durasiMinggu} Minggu`;
   }
-} else if (durasiBulan > 0) {
-  if (sisaHariBulan > 0) {
-    duration = `Durasi ${durasiBulan} Bulan ${sisaHari - 1} Hari`;
-  } else {
-    duration = `Durasi ${durasiBulan} Bulan`;
-  }
-} else if (sisaHari > 0) {
-  duration = `Durasi ${durasiMinggu} Minggu ${sisaHari} Hari`;
-} else {
-  duration = `Durasi ${durasiMinggu} Minggu`;
+
+  let currentTime = () => {
+    let currentDate = new Date();
+    let hours = currentDate.getHours();
+    let minutes = currentDate.getMinutes();
+    let seconds = currentDate.getSeconds();
+    let month = currentDate.toLocaleString("en-US", { month: "short" });
+
+    return month + " - " + hours + ":" + minutes + ":" + seconds;
+  };
+
+  // mengambil value checkbox
+
+  let {js, java, phyton, node} = req.body
+
+js === 'on';
+java === 'on';
+phyton === 'on';
+node === 'on';
+const checkbox = [js, java, phyton, node]
+
+if (js == 'on'){
+  js = true
 }
-let currentTime = () => {
-  let currentDate = new Date();
-  let hours = currentDate.getHours();
-  let minutes = currentDate.getMinutes();
-  let seconds = currentDate.getSeconds();
-  let month = currentDate.toLocaleString('en-US', {month: 'short'});
+if(java == 'on'){
+  java = true 
+}
+if(phyton == 'on'){
+  phyton = true
+}
+if (node === 'on'){
+  node = true
+}
 
-  return month + '-' + hours + ':' + minutes + ':' + seconds
-};
-
-// let selectedTechnologies = Array.isArray(Tecnologies) ? Technologies: [Technologies];
-
-
-const dataBlog ={
+const dataBlog = {
   pName,
-  startDate,
-  endDate,
+  duration,
   description,
-  Technologies: Array.isArray(Tecnologies) ? Technologies: [Technologies],
   duration,
   postAt: currentTime(),
+  checkbox,
+  js,
+  java,
+  phyton,
+  node,
 };
+console.log('dataBlog',data)
 data.unshift(dataBlog)
 
   res.redirect('blog')
@@ -137,15 +165,73 @@ function updateBlogView(req, res){
 }
 
 function updateBlog(req, res){
-  const { pName, description, id } = req.body
+  const { pName, description, id , startDate, endDate, js, java, phyton, node,} = req.body
+  let startDateValue = new Date(startDate);
+  let endDateValue = new Date(endDate);
 
-  console.log ('Id :', id)
-  console.log('pName: ', pName)
-  console.log('description :', description)
+  let durasiWaktu = endDateValue.getTime() - startDateValue.getTime();
+  let durasiHari = durasiWaktu / (1000 * 3600 * 24);
+  let durasiMinggu = Math.floor(durasiHari / 7);
+  let sisaHari = durasiHari % 7;
+
+  let durasiBulan = Math.floor(durasiHari / 30);
+  let sisaHariBulan = durasiHari % 30;
+
+  let durasiTahun = Math.floor(durasiHari / 365);
+  let sisaHariTahun = durasiHari % 365;
+
+  let duration = "";
+
+  if (durasiHari <= 6) {
+    duration = ` Durasi ${durasiHari + 1} Hari`;
+  } else if (durasiTahun > 0) {
+    if (sisaHariTahun > 0) {
+      duration = `Durasi ${durasiTahun} Tahun ${Math.floor(
+        sisaHariTahun / 30
+      )} Bulan ${Math.floor(sisaHariTahun % 30)} Hari`;
+    } else {
+      duration = `Durasi ${durasiTahun} Tahun`;
+    }
+  } else if (durasiBulan > 0) {
+    if (sisaHariBulan > 0) {
+      duration = `Durasi ${durasiBulan} Bulan ${sisaHari - 1} Hari`;
+    } else {
+      duration = `Durasi ${durasiBulan} Bulan`;
+    }
+  } else if (sisaHari > 0) {
+    duration = `Durasi ${durasiMinggu} Minggu ${sisaHari} Hari`;
+  } else {
+    duration = `Durasi ${durasiMinggu} Minggu`;
+  }
+
+  let currentTime = () => {
+    let currentDate = new Date();
+    let hours = currentDate.getHours();
+    let minutes = currentDate.getMinutes();
+    let seconds = currentDate.getSeconds();
+    let month = currentDate.toLocaleString("en-US", { month: "short" });
+
+    return month + " - " + hours + ":" + minutes + ":" + seconds;
+  };
+  // console.log ('Id :', id)
+  // console.log('pName: ', pName)
+  // console.log('description :', description)
+  const checkbox = 
+  js === 'on';
+  java === 'on';
+  phyton === 'on';
+  node === 'on';
 
   data[parseInt(id)] = {
     pName,
-    description
+    description,
+    duration,
+    postAt: currentTime(),
+    js,
+    java,
+    phyton,
+    node,
+    checkbox
   }
 res.redirect('/blog')
 }
@@ -153,7 +239,7 @@ function deleteBlog(req, res) {
   const { id } = req.params
 
 
-  data.splice(id,1)
+  data.splice(id, 1)
   res.redirect('/blog')
 }
 function blog (req,res){
@@ -168,3 +254,28 @@ app.listen(port, () => {
   console.log(`server berjalan di port ${port}`)
 })
 
+
+// // calculate duration
+// function calculateDuration(start_date, end_date) {
+//   const start = new Date(start_date);
+//   const end = new Date(end_date);
+//   const duration = end - start;
+
+//   const seconds = Math.floor(duration / 1000);
+//   const minutes = Math.floor(seconds / 60);
+//   const hours = Math.floor(minutes / 60);
+//   const days = Math.floor(hours / 24);
+//   const weeks = Math.floor(days / 7);
+//   const months = Math.floor(weeks / 4.345); // 4.345 minggu dalam sebulan rata-rata
+//   const years = Math.floor(months / 12);
+
+//   return {
+//     years,
+//     months: months % 12,
+//     weeks: weeks % 4.345,
+//     days: days % 7,
+//     hours: hours % 24,
+//     minutes: minutes % 60,
+//     seconds: seconds % 60,
+//   };
+// }
